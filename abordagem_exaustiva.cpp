83bdfb32eba3cf7d1390dfcefe27df66adcf39ef
sarahp31 <sarahapc@al.insper.edu.br>
2023-10-03 10:17:09 -0300

    adiconando comentarios

--- abordagem_exaustiva.cpp
+++ abordagem_exaustiva.cpp
@@ -4,9 +4,12 @@
 
 using namespace std;
 
+// Função para verificar se um subconjunto de vértices forma um clique
 bool is_clique(const vector<vector<bool>>& graph, const vector<int>& vertices) {
+    // Iterar sobre todos os pares de vértices do subconjunto
     for(int i = 0; i < vertices.size(); i++) {
         for(int j = i+1; j < vertices.size(); j++) {
+            // Se algum par de vértices não está conectado, o subconjunto não é um clique.
             if(!graph[vertices[i]][vertices[j]]) {
                 return false;
             }
@@ -15,11 +18,14 @@ bool is_clique(const vector<vector<bool>>& graph, const vector<int>& vertices) {
     return true;
 }
 
+// Função para encontrar o clique máximo no grafo
 vector<int> max_clique(const std::vector<std::vector<bool>>& graph) {
+    // Número de vértices no grafo
     int n = graph.size();
-    vector<int> max_clique_vertices;  // Vetor para armazenar os vértices do clique máximo.
+    // Vetor para armazenar os vértices do clique máximo
+    vector<int> max_clique_vertices;
 
-    // Iterar sobre todos os possíveis subconjuntos de vértices.
+    // Iterar sobre todos os possíveis subconjuntos de vértices
     for(int mask = 1; mask < (1 << n); mask++) {
         vector<int> vertices;
         for(int i = 0; i < n; i++) {
@@ -29,31 +35,37 @@ vector<int> max_clique(const std::vector<std::vector<bool>>& graph) {
         }
 
         // Se o subconjunto atual é um clique e é maior que o maior clique encontrado,
-        // atualizar max_clique_vertices.
+        // atualizar max_clique_vertices
         if(is_clique(graph, vertices) && vertices.size() > max_clique_vertices.size()) {
             max_clique_vertices = vertices;
         }
     }
-    return max_clique_vertices;  // Retornar os vértices do clique máximo encontrado.
+    // Retornar os vértices do clique máximo encontrado
+    return max_clique_vertices;
 }
 
 int main() {
+    // Abrir o arquivo para leitura
     ifstream file("grafo.txt");
 
     if(!file.is_open()) {
         cerr << "Erro ao abrir o arquivo." << endl;
         return 1;
     }
-
+    // Número de vértices e arestas
     int n_vertices, n_edges;
     file >> n_vertices >> n_edges;
 
+    // Criar matriz de adjacência para armazenar o grafo
     vector<vector<bool>> graph(n_vertices, vector<bool>(n_vertices, false));
 
+    // Ler as arestas do arquivo e preencher a matriz de adjacência.
     for(int i = 0; i < n_edges; i++) {
         int u, v;
         file >> u >> v;
+        // Ajustar os índices para começar de 0 e marcar u e v como conectados
         graph[u-1][v-1] = true;
+        // Como o grafo é não direcionado, marcar v e u como conectados também
         graph[v-1][u-1] = true;
     }
 
